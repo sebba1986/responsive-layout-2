9c7c3b091ca34e9e0d7baa1ddbd917c000f8bc35
sebba1986 <70055653+sebba1986@users.noreply.github.com>
2020-10-07 21:06:39 +0300

    seeMenuBtn not working

---
+++ resources/script.js
@@ -0,0 +1,7 @@
+const seeMenuBtn = document.getElementById("seeMenuBtn");
+const seeMenuSection = document.getElementById("seeMenuSection");
+const closeBtn = document.getElementById("closeButton");
+
+seeMenuBtn.addEventListener ('click', function() {seeMenuSection.classList.remove('hidden');})
+    
+
