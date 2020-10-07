2bd2b92743be30b9daaa9b905119cad10862d93c
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2020-10-07 21:19:14 +0300

    fix id name

--- resources/script.js
+++ resources/script.js
@@ -2,6 +2,9 @@ const seeMenuBtn = document.getElementById("seeMenuBtn");
 const seeMenuSection = document.getElementById("seeMenuSection");
 const closeBtn = document.getElementById("closeButton");
 
-seeMenuBtn.addEventListener ('click', function() {seeMenuSection.classList.remove('hidden');})
+seeMenuBtn.addEventListener ('click', function() {
+    console.log('click');
+    seeMenuSection.classList.remove('hidden');
+});
     
 
